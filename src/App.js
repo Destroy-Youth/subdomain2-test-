b7198f6060eecd641cd759890506bdaf1d128a9e
Ivan Herrera Equihua <luis.herrera@mymoons.mx>
2022-06-16 16:02:06 -0500

    iframes test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 
+const domains = ["http://localhost:3000", "http://localhost:3001"];
+
+window.addEventListener("message", messageHandler, false);
+
+function messageHandler(event) {
+  const { action, key, value } = event.data;
+  if (action == "returnData") {
+    console.log("host returns data", event.data);
+    localStorage.setItem("sessionFromAfar", value);
+    console.log(localStorage);
+  }
+}
+
 function App() {
+  function fetchData() {
+    const iframe = document.getElementById("ifr");
+    iframe.contentWindow.postMessage(
+      {
+        action: "get",
+        key: "session",
+      },
+      "*"
+    );
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+        <iframe id="ifr" src={domains[0]}></iframe>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={fetchData}>Get data from host</button>
+        <button onClick={() => console.log(localStorage)}>
+          print localStorage
+        </button>
       </header>
     </div>
   );
