d2bb887472b69a64248e7179774e33e0df6fb1f8
Ivan Herrera Equihua <luis.herrera@mymoons.mx>
2022-06-20 16:25:31 -0500

    base

--- src/App.js
+++ src/App.js
@@ -1,41 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect } from "react";
 import Cookies from "js-cookie";
-
-const domains = ["http://localhost:3000", "http://localhost:3001"];
-
-window.addEventListener("message", messageHandler, false);
-
-function messageHandler(event) {
-  const { action, key, value } = event.data;
-  if (action == "returnData") {
-    console.log("host returns data", event.data);
-    localStorage.setItem("sessionFromAfar", value);
-    console.log(localStorage);
-  }
-}
+import { useState } from "react";
 
 function App() {
-  function fetchData() {
-    const iframe = document.getElementById("ifr");
-    iframe.contentWindow.postMessage(
-      {
-        action: "get",
-        key: "session",
-      },
-      "*"
-    );
+  const [token, setToken] = useState();
+
+  useEffect(() => {
+    setToken(Cookies.get("token"));
+  }, []);
+
+  function logout() {
+    Cookies.remove("token");
   }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <button onClick={fetchData}>Get data from host</button>
-        <button onClick={() => console.log(Cookies.get("session"))}>
-          print coockie session
-        </button>
-      </header>
+      <h1>App 2</h1>
+      <h2>Token: {token}</h2>
+      <button onClick={logout}>Logout</button>
     </div>
   );
 }
